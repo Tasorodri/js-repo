35323fff5050274207f7495b69e25a86819ea27a
Tasorodri <tasorodri@gmail.com>
2021-11-30 19:37:52 +0100

    8

--- Ejercicio-08/Ejercicio8.js
+++ Ejercicio-08/Ejercicio8.js
@@ -14,14 +14,13 @@ class meteoHandler {
         this.cargarDatos()
     }
     cargarDatos(){
-        
         $.ajax({
             dataType: "json",
             url: this.url,
             method: 'GET',
             success: this.procesaDatos,
             error:function(){
-               $("#content").append(localerror);    
+               $("main").append(localerror);    
             }
         });
     }
@@ -29,21 +28,21 @@ class meteoHandler {
     procesaDatos(data){
         var iconcode = data.weather[0].icon;
        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-        $("#content").empty();
-        $("#content").append("<h3>Resumen:</h2>");
-        $("#content").append("<h3>" + data.name + " " + data.main.temp+ " " + data.weather[0].description + "<div id=\"icon\"><img id=\"wicon\" src=\"\" alt=\"Weather icon\" </h2>");
-        $("#content").append("<h3>Mas informacion:</h2>");
-       $("#content").append("<p>Ciudad: " + data.name + ", " +data.sys.country + "</p>");
-       $("#content").append("<p>Coordenadas (lat,long): " + data.coord.lat + ", " + data.coord.lon + " grados</p>");
-       $("#content").append("<p>Temperatura: " + data.main.temp + " ºC " + " maxima: " + data.main.temp_max + "ºC minima: " + data.main.temp_min +" ºC  </p>");
-       $("#content").append("<p>Presión: " + data.main.pressure + " hPa, Humedad: " + data.main.humidity + "% </p>");
-       $("#content").append("<p>Amanece a las: " + new Date(data.sys.sunrise *1000).toLocaleTimeString() + "</p>"); 
-       $("#content").append("<p>Oscurece a las: " + new Date(data.sys.sunset *1000).toLocaleTimeString() + "</p>"); 
-       $("#content").append("<p>Viento: Velocidad " + data.wind.speed + "direccion: " + data.wind.deg + "  grados</p>");
-       $("#content").append("<p>Medido a las: " + new Date(data.dt *1000).toLocaleTimeString() + " del: " +new Date(data.dt *1000).toLocaleDateString() + "</p>");
+        $("main").empty();
+        $("main").append("<h3>Resumen:</h2>");
+        $("main").append("<h3>" + data.name + " " + data.main.temp+ " " + data.weather[0].description + "<div id=\"icon\"><img id=\"wicon\" src=\"\" alt=\"Weather icon\" </h2>");
+        $("main").append("<h3>Mas informacion:</h2>");
+       $("main").append("<p>Ciudad: " + data.name + ", " +data.sys.country + "</p>");
+       $("main").append("<p>Coordenadas (lat,long): " + data.coord.lat + ", " + data.coord.lon + " grados</p>");
+       $("main").append("<p>Temperatura: " + data.main.temp + " ºC " + " maxima: " + data.main.temp_max + "ºC minima: " + data.main.temp_min +" ºC  </p>");
+       $("main").append("<p>Presión: " + data.main.pressure + " hPa, Humedad: " + data.main.humidity + "% </p>");
+       $("main").append("<p>Amanece a las: " + new Date(data.sys.sunrise *1000).toLocaleTimeString() + "</p>"); 
+       $("main").append("<p>Oscurece a las: " + new Date(data.sys.sunset *1000).toLocaleTimeString() + "</p>"); 
+       $("main").append("<p>Viento: Velocidad " + data.wind.speed + "direccion: " + data.wind.deg + "  grados</p>");
+       $("main").append("<p>Medido a las: " + new Date(data.dt *1000).toLocaleTimeString() + " del: " +new Date(data.dt *1000).toLocaleDateString() + "</p>");
        $('#wicon').attr('src', iconurl);
-       $("#content").append("<p>Visibilidad: " + data.visibility + " metros</p>");
-       $("#content").append("<p>Nubosidad: " + data.clouds.all + " %</p>");
+       $("main").append("<p>Visibilidad: " + data.visibility + " metros</p>");
+       $("main").append("<p>Nubosidad: " + data.clouds.all + " %</p>");
     }
  
       
